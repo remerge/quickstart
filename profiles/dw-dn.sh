7a4a341cb22b724a0ec16a60ba33b314283f2f7d
Benedikt Böhm <bb@xnull.de>
2014-08-02 18:37:05 +0200

    add dw-dn profile

---
+++ profiles/dw-dn.sh
@@ -0,0 +1,13 @@
+. profiles/common/base.sh
+
+part sdc 1 fd00 32G
+part sdc 2 a504
+
+part sdd 1 fd00 32G
+part sdd 2 a504
+
+mdraid md1 --level=1 --raid-devices=2 /dev/sdc1 /dev/sdd1
+
+format /dev/md1 ext4
+
+mountfs /dev/md1 ext4 /
